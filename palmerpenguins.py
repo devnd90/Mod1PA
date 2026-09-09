fb486eed1f70ecca8f7eded6ab2f501fa7f24438
devnd90 <devin.dollard@gmail.com>
2026-09-09 09:48:38 -0400

    Add files via upload

--- palmerpenguins.py
+++ palmerpenguins.py
@@ -3,15 +3,16 @@
 # 9/6/2026
 # Introducing the Palmer Penguins
 
-# Define the penguin species
-p1 = "Chinstrap"
-p2 = "Gentoo"
-p3 = "Adelie"
+# Define constants for the penguin species
+CHINSTRAP = "Chinstrap"
+GENTOO = "Gentoo"
+ADELIE = "Adelie"
+TOTAL_SPECIES = 3
 
 # Print the formatted output
 print("Introducing the Palmer Penguins:")
-print(f"\t{p1}!")
-print(f"\t{p2}!")
+print("\t" + CHINSTRAP + "!")
+print("\t" + GENTOO + "!")
 print("and last but not least...")
-print(f"\t{p3}!")
-print("There are a total of 3 penguin species in this dataset.")
+print("\t" + ADELIE + "!")
+print("There are a total of", TOTAL_SPECIES, "penguin species in this dataset.")
